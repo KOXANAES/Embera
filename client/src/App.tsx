148dfc90eaaa78c19c0b475c49888e0fa6535685
koxanaes <koxanaes@mail.ru>
2024-06-15 20:21:10 +0300

    Подключил login, logout, registration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,64 @@
-function App() {
+import {FC, useContext, useEffect, useState} from "react";
+import LoginForm from "./components/LoginForm";
+import { Context } from "./main";
+import { observer } from "mobx-react-lite";
+import { IUser } from "./models/IUser";
+import UserService from "./service/UserService";
+
+const App: FC = () => {
+
+  const [users, setUsers] = useState<IUser[]>([])
+
+  const {store} = useContext(Context)
+  useEffect(() => { 
+    if(localStorage.getItem('token')) { 
+      store.checkAuth()
+    }
+  }, [])
+
+  async function getUsers(){ 
+    try { 
+      const response = await UserService.fetchUsers()
+      setUsers(response.data)
+    } catch(e) { 
+      console.log(e)
+    }
+  }
+
+  if(store.isLoading) { 
+    return(
+      <div>Загрузка...</div>
+    )
+  }
+
+  if(!store.isAuth) { 
+    return(
+      <div>
+        <LoginForm/>
+        <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+      {users.map(user => 
+        <div key={user.email}>{user.email}</div>
+      )}
+      </div>
+              
+    )
+  }
+
   return (
-    <div>
-      132
+    <div className="App">
+      <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : `Не авторизован`}</h1>
+      <h1>{store.user.isActivated ? `Аккаунт активирован ${store.user.email}` : `Активируйте аккаунт, иначе некоторые опции сервиса будут недоступны`}</h1>
+      <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+      {users.map(user => 
+        <div key={user.email}>{user.email}</div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default observer(App);
---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,36 @@
+import { FC, useContext, useState } from "react";
+import { Context } from "../main";
+import { observer } from "mobx-react-lite";
+
+const LoginForm: FC = () => { 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+    const {store} = useContext(Context)
+    return(
+        <div>
+            <input 
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+            type="text" 
+            placeholder="Email"
+            />
+            <input 
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+            type="password" 
+            placeholder="Пароль"
+            />
+            <input 
+            onChange={e => setUsername(e.target.value)}
+            value={username}
+            type="text" 
+            placeholder="Ник пользователя"
+            />
+            <button onClick={() => store.login(email, password)}>Логин</button>
+            <button onClick={() => store.registration(email, password, username)}>Регистрация</button>
+        </div>        
+    )
+}
+
+export default observer(LoginForm)
