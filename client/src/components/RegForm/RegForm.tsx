901019e884807dc5a72dd8bf2ef339b6271ed163
koxanaes <koxanaes@mail.ru>
2024-06-16 19:26:03 +0300

    Реализовал навбар. Приступаю к browser router

--- client/src/components/RegForm/RegForm.tsx
+++ client/src/components/RegForm/RegForm.tsx
@@ -18,24 +18,27 @@ const RegForm:FC<RegForm> = ({active, setActive}) => {
     <div className={active ? 'regForm active' : 'regForm'} onClick={() => setActive(false)}>
       <div className={active ? 'regForm_content active' : 'regForm_content'} onClick={e => e.stopPropagation()}>
           <input
+            className='auth_input'
             onChange={e => setEmail(e.target.value)}
             value={email}
             type="text" 
             placeholder="Email"
             />
             <input
+            className='auth_input'
             onChange={e => setPassword(e.target.value)}
             value={password}
             type="password" 
             placeholder="Пароль"
             />
             <input
+            className='auth_input'
             onChange={e => setUsername(e.target.value)}
             value={username}
             type="text" 
             placeholder="Ник пользователя"
             />
-            <button onClick={() => store.registration(email, password, username)}>Регистрация</button>
+            <button className='login_button' onClick={() => store.registration(email, password, username)}>Регистрация</button>
       </div>
     </div>
   )
